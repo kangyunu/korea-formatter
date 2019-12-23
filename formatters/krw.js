5bf8eb094fdf86a8d61ddfcf3008c6a5af3c1999
KangByungSeon <rkdqudtjs1@naver.com>
2019-12-23 17:47:20 +0900

    krw - 처리

--- formatters/krw.js
+++ formatters/krw.js
@@ -1,5 +1,5 @@
 module.exports = value => {
-  const str = (value + "").replace(/[^0-9]/g, "");
+  const str = (value + "").replace(/[^0-9-]/g, "");
 
   return new Intl.NumberFormat('ko-KR').format(str) + "원"
 }
