fd17342a475e5ba1ad9f7fe715f218c835fbaf05
KangByungSeon <rkdqudtjs1@naver.com>
2020-03-02 14:41:01 +0900

    krw 단위 옵션 추가

--- formatters/krw.js
+++ formatters/krw.js
@@ -1,5 +1,4 @@
-module.exports = function (value) {
+module.exports = function (value, options = { unit: true }) {
   const str = (value + "").replace(/[^0-9-]/g, "");
-
-  return new Intl.NumberFormat('ko-KR').format(str) + "원"
+  return `${new Intl.NumberFormat('ko-KR').format(str)}${options.unit ? "원" : ""}`
 }
